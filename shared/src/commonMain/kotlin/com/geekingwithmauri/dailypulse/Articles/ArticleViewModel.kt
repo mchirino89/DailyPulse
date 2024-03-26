4617a9e1d591ae9cf6d352af624fda64a1fd0e2c
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2024-03-26 17:11:50 -0300

    Add mock network behavior

--- shared/src/commonMain/kotlin/com/geekingwithmauri/dailypulse/Articles/ArticleViewModel.kt
+++ shared/src/commonMain/kotlin/com/geekingwithmauri/dailypulse/Articles/ArticleViewModel.kt
@@ -7,7 +7,9 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class ArticleViewModel: BaseViewModel() {
-    private val _articleState: MutableStateFlow<ArticleState> = MutableStateFlow(ArticleState())
+    private val _articleState: MutableStateFlow<ArticleState> = MutableStateFlow(
+        ArticleState(loading = true)
+    )
 
     val articleState: StateFlow<ArticleState> get() = _articleState
 
@@ -17,8 +19,32 @@ class ArticleViewModel: BaseViewModel() {
 
     private fun getArticles() {
         scope.launch {
+            val fetchedArticles = fetchArticles()
             delay(500)
-            _articleState.emit(ArticleState())
+            _articleState.emit(ArticleState(articles = fetchedArticles))
         }
     }
+
+    suspend fun fetchArticles(): List<Article> = mockArticle
+
+    private val mockArticle = listOf(
+        Article(
+            title = "Title 1",
+            description = "Description 1",
+            date = "2021-09-01",
+            imageUrl = "https://picsum.photos/200/300"
+        ),
+        Article(
+            title = "Title 2",
+            description = "Description 2",
+            date = "2021-09-02",
+            imageUrl = "https://picsum.photos/200/300"
+        ),
+        Article(
+            title = "Title 3",
+            description = "Description 3",
+            date = "2021-09-03",
+            imageUrl = "https://picsum.photos/200/300"
+        )
+    )
 }
