6ec2da28f332c32cb8d65704f65e3cbe33ab41d0
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2024-03-26 16:59:11 -0300

    Add initial get article async scaffolding

--- shared/src/commonMain/kotlin/com/geekingwithmauri/dailypulse/Articles/ArticleViewModel.kt
+++ shared/src/commonMain/kotlin/com/geekingwithmauri/dailypulse/Articles/ArticleViewModel.kt
@@ -1,11 +1,24 @@
 package com.geekingwithmauri.dailypulse.Articles
 
 import com.geekingwithmauri.dailypulse.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ArticleViewModel: BaseViewModel() {
     private val _articleState: MutableStateFlow<ArticleState> = MutableStateFlow(ArticleState())
 
     val articleState: StateFlow<ArticleState> get() = _articleState
+
+    init {
+        getArticles()
+    }
+
+    private fun getArticles() {
+        scope.launch {
+            delay(500)
+            _articleState.emit(ArticleState())
+        }
+    }
 }
